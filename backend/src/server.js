a9cdd718a916c98df8b296b1b6046655daaf8736
Mihretab Ayele <mihreayele@gmail.com>
2026-03-14 21:43:39 +0100

    convert backend to vercel API routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,4 @@
-//const express = require("express")
-import express from "express";
-import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import path from "path";
-dotenv.config();
-const app = express();
+import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
-
-const __dirname = path.resolve();
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
-
-//make ready to deployment
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 app.listen(PORT, () => console.log("Server running on port: " + PORT));
