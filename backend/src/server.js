0a55915a153f99337d1ffff4830351e208392d01
Mihretab Ayele <mihreayele@gmail.com>
2026-03-24 15:13:46 +0100

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,13 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import path from "path";
+import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); //req.body
 
 const __dirname = path.resolve();
 app.use("/api/auth", authRoutes);
@@ -20,4 +23,19 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-app.listen(PORT, () => console.log("Server running on port: " + PORT));
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log("Server running on port: " + PORT);
+    });
+  } catch (error) {
+    console.error(
+      "Server startup aborted because the database is unavailable.",
+    );
+    process.exit(1);
+  }
+};
+
+startServer();
