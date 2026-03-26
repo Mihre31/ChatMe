3e41f7fdb966d26997545fc8dbb7a90260cafa6b
Mihretab Ayele <mihreayele@gmail.com>
2026-03-26 19:12:36 +0100

    update profile endpoint added

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,12 +6,14 @@ import messageRoutes from "./routes/message.route.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json()); //req.body
+app.use(cookieParser());
 
 const __dirname = path.resolve();
 app.use("/api/auth", authRoutes);
